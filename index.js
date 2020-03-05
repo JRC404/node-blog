a953c876dd58de3e7b8f5241dac4b7b663d74bf5
Jacob <jacob@Jacobs-MBP.lan>
2020-03-05 21:40:36 +0000

    Mongoose and Body parser installed. Readme to be updated.

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const app = express();
 
+// mongoose
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/node-blog");
+
+// body parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 // engine setup
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
