0774c6886bb2d9042390b95347a9e62019a9cfc7
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-03-30 12:48:33 +0100

    MongoDB connection added.

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
-const request = require("request");
 const path = require("path");
 const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const BlogSchema = require("./models/blog");
+
+mongoose.connect(
+  `mongodb+srv://${process.env.Username}:${process.env.Password}@cluster0-4fdtu.mongodb.net/blogInfo?retryWrites=true&w=majority`,
+  {
+    // these two are something to look into... more information will follow but right now... let's code.
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 const app = express();
 const getPost = require("./lib/getPost");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  bodyParser.urlencoded({
+    // kirsty saved the day :-)
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
 app.engine(
   ".hbs",
@@ -19,6 +39,21 @@ app.engine(
 app.set("view engine", ".hbs");
 
 app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  let blogTitle = req.body.blogTitle;
+  let blogContent = req.body.blogContent;
+  let blogAuthor = req.body.blogAuthor;
+
+  const newBlog = new BlogSchema({
+    blogTitle: blogTitle,
+    blogContent: blogContent,
+    blogAuthor: blogAuthor
+  });
+
+  newBlog.save();
   res.render("index");
 });
 
---
+++ models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  blogTitle: {
+    type: String,
+    required: true
+  },
+  blogContent: {
+    type: String,
+    required: true
+  },
+  blogAuthor: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("blogs", blogSchema);
