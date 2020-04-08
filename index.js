c9168930c8c17a3091de24e1934612066b0b8e14
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-04-08 12:27:52 +0100

    ESLint installed. Running through bugs etc

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const path = require("path");
-const hbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(`${process.env.DatabaseURL}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const app = express();
-const index = require("./routes/index");
+const index = require('./routes/index');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   bodyParser.urlencoded({
-    extended: false
-  })
+    extended: false,
+  }),
 );
 app.use(bodyParser.json());
 
 app.engine(
-  ".hbs",
+  '.hbs',
   hbs({
-    defaultLayout: "layout",
-    extname: ".hbs"
-  })
+    defaultLayout: 'layout',
+    extname: '.hbs',
+  }),
 );
 
-app.set("view engine", ".hbs");
+app.set('view engine', '.hbs');
 
-app.use("/", index);
+app.use('/', index);
 app.use((req, res, next) => {
   if (res.status(404)) {
-    res.render("404");
+    res.render('404');
     return;
   }
   next();
 });
 
 app.listen(process.env.PORT || 3005, () => {
-  console.log("I am listening on port 3005.");
+  console.log('I am listening on port 3005.');
 });
