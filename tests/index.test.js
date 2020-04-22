1757a3308d6a7533bcb7b9b3393af3a2245b7c98
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-04-22 23:08:28 +0100

    Supertest installed and first test created.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,21 @@
-it('Testing to see if Jest works', () => {
-  expect(1).toBe(2);
+const app = require('./server');
+const supertest = require('supertest');
+const request = supertest(app);
+
+app.get('/test', async (req, res) => {
+  res.json({ message: 'pass!' });
 });
+
+it('Gets the /test endpoint', async (done) => {
+  const res = await request.get('/test');
+
+  expect(res.status).toBe(200);
+  expect(res.body.message).toBe('pass!');
+  done();
+});
+
+// it('is an asyncronous test', async (done) => {
+//   // something
+
+//   done();
+// });
